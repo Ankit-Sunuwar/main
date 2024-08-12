cf555642c857848b40a5c86f59dae522d1db5414
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-08-12 15:25:21 +0545

    learned about the room.routes

---
+++ DynamicRoute/modules/rooms/room.route.js
@@ -0,0 +1,45 @@
+const router = require("express").Router();
+
+//Read all data
+router.get("/", (req, res) => {
+  //query
+  console.log(req?.query);
+  res.json({ data: "Hello from users id" });
+});
+
+//Read one data
+router.get("/:id", (req, res) => {
+  //params
+  res.json({ data: `get data from user id ${req?.params?.id}` });
+});
+
+//Create
+router.post("/", (req, res) => {
+  // body
+  console.log(req?.body);
+  res.json({ data: "Hello from post user" });
+});
+
+//Update
+router.put("/:id", (req, res) => {
+  //params + body
+  console.log(req?.body);
+  res.json({ data: `put data from user id ${req?.params?.id}` });
+});
+
+//Update
+router.patch("/:id", (req, res) => {
+  //params + body
+  console.log(req?.body);
+  res.json({
+    data: `patch data from user id ${req?.params?.id} `,
+  });
+});
+
+//Delete
+router.delete("/:id", (req, res) => {
+  //params
+  res.json({ data: `delete data from user id ${req?.params?.id}` });
+});
+
+module.exports = router;
--- DynamicRoute/routes/index.js
+++ DynamicRoute/routes/index.js
@@ -3,4 +3,7 @@ const router = require("express").Router();
 const userRouter = require("../modules/users/user.route");
 router.use("/users", userRouter);
 
+const roomRouter = require("../modules/rooms/room.route");
+router.use("/rooms", roomRouter);
+
 module.exports = router;
