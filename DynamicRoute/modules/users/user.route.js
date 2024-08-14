fc8ebf3a27f22e9e1862e49420bdb4fde58037ef
Ankit-Sunuwar <ankitsunuwar171@gmail.com>
2024-08-14 15:54:02 +0545

    learned about RBAC(role based access control)

--- DynamicRoute/modules/users/user.route.js
+++ DynamicRoute/modules/users/user.route.js
@@ -1,16 +1,32 @@
 const router = require("express").Router();
-const multer = require("multer");
+// const multer = require("multer");
 
-const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "public/uploads");
-  },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + "." + file?.originalname.split(".")[1]);
-  },
-});
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "public/uploads");
+//   },
+//   filename: function (req, file, cb) {
+//     cb(null, Date.now() + "." + file?.originalname.split(".")[1]);
+//   },
+// });
 
-const upload = multer({ storage });
+// const upload = multer({ storage });
+
+// RBAC(role based access control)
+const checkRole = (sysRoles = []) => {
+  // system lai chaine role
+  return (req, res, next) => {
+    try {
+      const { roles: userRole = [] } = req.headers; //user role
+      console.log({ userRole });
+      const isValidRole = sysRoles.some((role) => userRole.includes(role));
+      if (!isValidRole) throw new Error("User unauthorized");
+      next();
+    } catch (e) {
+      next(e);
+    }
+  };
+};
 
 const verify = (req, res, next) => {
   const role = req.headers.role;
@@ -21,17 +37,8 @@ const verify = (req, res, next) => {
   }
 };
 
-const verify2 = (req, res, next) => {
-  const role = req.headers.roles;
-  if (role === "user") {
-    next();
-  } else {
-    throw new Error("You are not authorized");
-  }
-};
-
 //Read all data
-router.get("/", verify, verify2, (req, res, next) => {
+router.get("/:id", verify, checkRole(["admin"]), (req, res, next) => {
   //query
   // try,catch,throw ehni haru sabai error handling haru ho
   try {
@@ -42,7 +49,7 @@ router.get("/", verify, verify2, (req, res, next) => {
 });
 
 //Read one data
-router.get("/:id", (req, res, next) => {
+router.get("/", checkRole(["admin", "user"]), (req, res, next) => {
   //params
   try {
     res.json({ data: `get data from user id ${req?.params?.id}` });
@@ -52,57 +59,58 @@ router.get("/:id", (req, res, next) => {
 });
 
 //Create
-router.post("/", (req, res, next) => {
-  // body
-  try {
-    console.log(req?.body);
-    res.json({ data: "Hello data from user" });
-  } catch (e) {
-    next(e);
-  }
-});
+// router.post("/:id", (req, res, next) => {
+//   // body
+//   try {
+//     console.log(req?.body);
+//     res.json({ data: "Hello data from user" });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
 
-router.post("/register", upload.single("profilePic"), (req, res, next) => {
-  try {
-    console.log({ pic: req?.file });
-    res.json({ data: "User Registered Sucessfully" });
-  } catch (e) {
-    next(e);
-  }
-});
+// router.post("/register", upload.single("profilePic"), (req, res, next) => {
+//   try {
+//     const URL = "http://localhost:5555/resources//uploads/"; // yo developer's haru le photo ko url banauna user gar xa.
+//     const myPicture = URL + req?.file?.filename;
+//     res.json({ data: `User Registered Sucessfully in url ${myPicture}` });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
 
-//Update
-router.put("/:id", (req, res) => {
-  //params + body
-  try {
-    console.log(req?.body);
-    res.json({ data: `put data from user id ${req?.params?.id}` });
-  } catch (e) {
-    next(e);
-  }
-});
+// //Update
+// router.put("/:id", (req, res) => {
+//   //params + body
+//   try {
+//     console.log(req?.body);
+//     res.json({ data: `put data from user id ${req?.params?.id}` });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
 
-//Update
-router.patch("/:id", (req, res) => {
-  //params + body
-  try {
-    console.log(req?.body);
-    res.json({
-      data: `patch data from user id ${req?.params?.id} `,
-    });
-  } catch (e) {
-    next(e);
-  }
-});
+// //Update
+// router.patch("/:id", (req, res) => {
+//   //params + body
+//   try {
+//     console.log(req?.body);
+//     res.json({
+//       data: `patch data from user id ${req?.params?.id} `,
+//     });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
 
-//Delete
-router.delete("/:id", (req, res) => {
-  //params
-  try {
-    res.json({ data: `delete data from user id ${req?.params?.id}` });
-  } catch (e) {
-    next(e);
-  }
-});
+// //Delete
+// router.delete("/:id", (req, res) => {
+//   //params
+//   try {
+//     res.json({ data: `delete data from user id ${req?.params?.id}` });
+//   } catch (e) {
+//     next(e);
+//   }
+// });
 
 module.exports = router;
